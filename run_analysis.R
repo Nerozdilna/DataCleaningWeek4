98b173b8e9fc926c4024e5ab570fcb2c9a0d053a
Nerozdilna <Nerozdilna@users.noreply.github.com>
2017-03-18 14:45:17 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,5 @@
 ### Step_1
 ### Preparation of dataset folder
-
 fileurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 #Downloading dataset zip from provided url
 download.file(fileurl, "./assignment.zip") 
@@ -11,14 +10,15 @@ unzip("assignment.zip", exdir = ".")
 ### Step_2
 ### Reading subject codes, activity labels and observation files from "test" subfolder, and merging three pieces into one dataset
 
-subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", header= FALSE) 
-### Subjects vary from 1 to 30, dimention of this set is 2947 by 1
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", header= FALSE) ### Subjects vary from 1 to 30, dimention of this set is 2947 by 1
+### Subject codes vary from 1 to 30, dimention of this set is 2947 by 1
 label_test <- read.table("./UCI HAR Dataset/test/y_test.txt", header= FALSE)
 ### Activity labels vary from 1 to 6, dimention of this set is also 2947 by 1
 obs_test <- read.table("./UCI HAR Dataset/test/X_test.txt", header= FALSE)
 ### This is a set of observations with dimentions 2947 by 561
 
 ### We will now merge above datasets
+
 test <- cbind(subject_test, label_test, obs_test)
 
 ### Step_3
@@ -30,6 +30,7 @@ obs_train <- read.table("./UCI HAR Dataset/train/X_train.txt", header= FALSE)
 train <- cbind(subject_train, label_train, obs_train)
 
 ### Merging two subsets
+
 completedataset <- rbind(test, train)
 ### This resulting dataset has dimentions of 10299 by 563.
 
@@ -42,6 +43,8 @@ library(stringi)
 features <- stri_read_lines("./UCI HAR Dataset/features.txt")
 ### To make header look tidier, we will remove 1:561 indices from variable names 
 features_string <- c(stri_sub(features[1:9], 3), stri_sub(features[10:99], 4), stri_sub(features[100:561], 5))
+features_string <- sub("BodyBody", "Body", features_string)
+### Removing "double body" in a couple of variables
 
 ### We will now provide headers to completedataset
 names(completedataset) <- c("subject", "activitycode", features_string)
@@ -58,10 +61,11 @@ filterdataset <- completedataset[ ,c(1:2, string_filtered)]
 ### Step_5
 ### Preparing summarized data table
 
-summarised <- aggregate(filterdataset[ ,3:66], list(filterdataset$subject, filterdataset$activitycode), mean)
+summarised <- aggregate(filterdataset[ ,3:68], list(filterdataset$subject, filterdataset$activitycode), mean)
 ### Note: [3:69] subsetting is conducted because first two columns of dataset are summarizing factors for this data summary
 
 summarised <- dplyr::rename(summarised, subject = Group.1, activitycode = Group.2)
+
 ### Renaming gives titles to first two columns.
 
 ### Step_6
